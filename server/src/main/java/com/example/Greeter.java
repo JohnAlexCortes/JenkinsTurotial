83fd0dd501102b5b93341009ffed92eeedfe153b
John Alex Cortes <johnalex8411@hotmail.com>
2019-05-21 21:31:41 -0500

    Se ingresa este cambio para que se vea reflejado en la tareaa programada desde Jenkins para la compilación automatica

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello Mundo cruel, %s!", someone);
+    return String.format("Hello Mundo cruel yyyyy, %s!", someone);
   }
 }
