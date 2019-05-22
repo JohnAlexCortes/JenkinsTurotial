ac32f0c88044407b652599531d726a1467c26826
John Alex Cortes <johnalex8411@hotmail.com>
2019-05-21 22:28:36 -0500

    Cambio para corregir algunas alertas

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * @param 
+   * @return 
+   */
+  public final String greet(final String someone) {
     return String.format("eeee  Hello Mundo cruel yyyyy, %s!", someone);
   }
 }
